383b9e5a77f256d04bf93c565158ac8dabad4cf4
Juliant Daniel Latuary <daniel.latuary@gmail.com>
2022-08-17 20:27:21 +0700

    add side

---
+++ resources/views/layout/adminlayout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('layout.partials.head')
+</head>
+
+<body>
+    @include('layout.partials.side')
+    @yield('content')
+</body>
+
+</html>
---
+++ resources/views/layout/partials/side.blade.php
@@ -0,0 +1,50 @@
+<nav class="text-center" id="sidebar">
+    <div class="sidebar-header">
+        <img src="assets/img/logopelindo.png" alt="" width="154.5" height="71">
+    </div>
+    <ul class="list-unstyled components ">
+        <li>
+            <a href="dashboard.php">Dashboard</a>
+        </li>
+        <li>
+            <a href="#notakapal" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Berita Acara Nota
+                Kapal</a>
+            <ul class="collapse list-unstyled" id="notakapal">
+                <li>
+                    <a href="banotakapal.php">Keluhan Berita Acara
+                        Nota Kapal</a>
+                </li>
+                <li>
+                    <a href="buatbanotakapal.php">Membuat Berita Acara Nota Kapal</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#notasampah" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Berita Acara Nota
+                Sampah Kapal</a>
+            <ul class="collapse list-unstyled" id="notasampah">
+                <li>
+                    <a href="basampahkapal.php">Keluhan Berita Acara Nota Sampah Kapal</a>
+                </li>
+                <li>
+                    <a href="buatbasampahkapal.php">Membuat Berita Acara Nota Sampah Kapal</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#hapusppkb" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Berita Acara
+                Penghapusan PPKB</a>
+            <ul class="collapse list-unstyled" id="hapusppkb">
+                <li>
+                    <a href="bahapusppkb.php">Keluhan Berita Acara Penghapusan PPKB</a>
+                </li>
+                <li>
+                    <a href="buatbahapusppkb.php">Membuat Berita Acara Penghapusan PPKB</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="index.php">Logout</a>
+        </li>
+    </ul>
+</nav>
