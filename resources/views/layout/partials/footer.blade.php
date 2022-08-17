8f2eb383c215e2b33a70141c60b646507a58c439
Juliant Daniel Latuary <daniel.latuary@gmail.com>
2022-08-17 20:13:31 +0700

    fix layout

---
+++ resources/views/layout/partials/footer.blade.php
@@ -0,0 +1,69 @@
+<footer class="text-center text-lg-start text-light" style="background: #3A4F6E;">
+    <!-- Section: Links  -->
+    <section class="">
+        <div class="container text-center text-md-start mt-0">
+            <!-- Grid row -->
+            <div class="row mt-0">
+                <!-- Grid column PT PELABUHAN -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-md-0 mb-0">
+                    <!-- Content -->
+                    <p class="pt-2 mt-2">
+                        <strong>BERLANGGANAN NEWSLETTER</strong>
+                    </p>
+                    <!-- Email input -->
+                    <div class="form-outline mb-4">
+                        <input type="email" id="form5Example2" class="form-control"
+                            placeholder="Masukan alamat email" />
+                    </div>
+                </div>
+
+                <!-- Grid column NEWSLETTER-->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-md-0 mb-0">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mt-4 mb-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-building" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z" />
+                            <path
+                                d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z" />
+                        </svg> PT Pelabuhan Persero II
+                    </h6>
+                    <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-map" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M15.817.113A.5.5 0 0 1 16 .5v14a.5.5 0 0 1-.402.49l-5 1a.502.502 0 0 1-.196 0L5.5 15.01l-4.902.98A.5.5 0 0 1 0 15.5v-14a.5.5 0 0 1 .402-.49l5-1a.5.5 0 0 1 .196 0L10.5.99l4.902-.98a.5.5 0 0 1 .415.103zM10 1.91l-4-.8v12.98l4 .8V1.91zm1 12.98 4-.8V1.11l-4 .8v12.98zm-6-.8V1.11l-4 .8v12.98l4-.8z" />
+                        </svg> Jl.Raya Pelabuhan No.9, Tanjung Priok
+                        Jakarta Utara,14310</p>
+                </div>
+
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-0">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mt-4 mb-4">
+                        <img src="https://img.icons8.com/dotty/20/ffffff/add-contact-to-company.png" /> Contact
+                    </h6>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-mailbox2" viewBox="0 0 16 16">
+                        <path d="M9 8.5h2.793l.853.854A.5.5 0 0 0 13 9.5h1a.5.5 0 0 0 .5-.5V8a.5.5 0 0 0-.5-.5H9v1z" />
+                        <path
+                            d="M12 3H4a4 4 0 0 0-4 4v6a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V7a4 4 0 0 0-4-4zM8 7a3.99 3.99 0 0 0-1.354-3H12a3 3 0 0 1 3 3v6H8V7zm-3.415.157C4.42 7.087 4.218 7 4 7c-.218 0-.42.086-.585.157C3.164 7.264 3 7.334 3 7a1 1 0 0 1 2 0c0 .334-.164.264-.415.157z" />
+                    </svg>
+                    corp_sec@indonesiaport.co.id
+                    </p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-telephone" viewBox="0 0 16 16">
+                        <path
+                            d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+                    </svg> 1500 767</p>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        Copyright ©
+        <a class="text-reset fw-bold" href="#">IPC Pelindo 2021</a>
+    </div>
+    <!-- Copyright -->
+</footer>
---
+++ resources/views/layout/partials/nav.blade.php
@@ -0,0 +1,51 @@
+<div class="container">
+    <!-- Navbar -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light" style="background: #F7AD1A">
+        <div class="container ">
+            <a class="navbar-brand" href="homeuser.php">
+                <img src="assets/img/logopelindo.png" alt="" width="108.8" height="50">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="col-7 justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="homeuser.php">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Keluhan
+                        </a>
+                        <ul class="dropdown-menu bg-primary" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item text-light" href="formnotakapal.php">Form Keluhan Nota
+                                    Kapal</a></li>
+                            <li><a class="dropdown-item text-light" href="formnotasampah.php">Form Keluhan Nota
+                                    Sampah Kapal</a></li>
+                            <li><a class="dropdown-item text-light" href="formppkb.php">Form Keluhan Penghapusan
+                                    PPKB</a></li>
+                        </ul>
+                    </li>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="hasil.php">Hasil</a>
+                    </li>
+                    <div class="col-5"> </div>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Selamat Datang
+                        </a>
+                        <ul class="dropdown-menu bg-primary" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item text-light" href="edit.php">Edit Profil</a></li>
+                            <li><a class="dropdown-item text-light" href="index.php">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
