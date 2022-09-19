ce3a5d7b06ed3fc93c3ea18bdaa010e02e7871f7
Juliant Daniel Latuary <daniel.latuary@gmail.com>
2022-09-19 10:05:02 +0700

    tampilan

--- resources/views/admin/nota-kapal/berita-acara/edit.blade.php
+++ resources/views/admin/nota-kapal/berita-acara/edit.blade.php
@@ -10,17 +10,15 @@
         Kembali
     </a>
 
-    <form action="{{ route('admin.nota-kapal.berita-acara.update', $beritaAcara->id) }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ route('admin.nota-kapal.berita-acara.update', $beritaAcara->id) }}" method="POST"
+        enctype="multipart/form-data">
         @csrf
         <div class="form-group mb-3 row">
-            {{-- <label class="col-sm-2 col-form-label" for="nomor_surat">Nomor Surat</label>
-            <div class="col-sm-4">
-                <input type="text" class="form-control" id="nomor_surat" name="nomor_surat" placeholder="Nomor Surat">
-            </div> --}}
             <label class="col-sm-2 col-form-label" for="nomor_nota_kapal">Nomor Nota Kapal</label>
             <div class="col-sm-4">
                 <select class="form-select" name="nota_id" disabled>
-                    <option selected disabled value="{{ $beritaAcara->nota_id }}">{{ $beritaAcara->nota->namakapal }}</option>
+                    <option selected disabled value="{{ $beritaAcara->nota_id }}">{{ $beritaAcara->nota->namakapal }}
+                    </option>
                     @foreach ($notaKapal as $d)
                         <option value="{{ $d->id }}">{{ $d->namakapal }}</option>
                     @endforeach
@@ -28,8 +26,8 @@
             </div>
             <label class="col-sm-2 col-form-label" for="tanggal">Tanggal</label>
             <div class="col-sm-4">
-                <input type="date" class="form-control" id="tanggal" name="tanggal" 
-                    placeholder="Tanggal" value="{{ $beritaAcara->tanggal->format('Y-m-d') }}" required>
+                <input type="date" class="form-control" id="tanggal" name="tanggal" placeholder="Tanggal"
+                    value="{{ $beritaAcara->tanggal->format('Y-m-d') }}" required>
             </div>
         </div>
         <div class="form-group mb-3 row">
@@ -52,31 +50,21 @@
             </div>
             <label class="col-sm-2 col-form-label" for="perihal">Perihal</label>
             <div class="col-sm-4">
-                <input type="text" class="form-control" id="perihal" name="perihal" 
-                    placeholder="Perihal" value="{{ $beritaAcara->perihal }}" required>
+                <input type="text" class="form-control" id="perihal" name="perihal" placeholder="Perihal"
+                    value="{{ $beritaAcara->perihal }}" required>
             </div>
         </div>
         <div class="form-group mb-3 row">
-            {{-- <label class="col-sm-2 col-form-label" for="nomor_nota_kapal">Nomor Nota Kapal</label>
-            <div class="col-sm-4">
-                <select class="form-select" name="nota_id">
-                    <option name="">Pilih nota kapal</option>
-                    @foreach ($notaKapal as $d)
-                        <option id="{{ $d->id }}" value="{{ $d->id }}" name="nota_id">
-                            {{ $d->namakapal }}</option>
-                    @endforeach
-                </select>
-            </div> --}}
             <label class="col-sm-2 col-form-label" for="lampiranpendukung">Lampiran Pendukung</label>
             <div class="col-sm-4">
-                <input type="file" class="form-control" id="lampiranpendukung" name="lampiranpendukung" 
+                <input type="file" class="form-control" id="lampiranpendukung" name="lampiranpendukung"
                     laceholder="Lampiran Pendukung" accept=".pdf">
                 <div class="form-text">Kosongkan jika tidak ingin mengubah.</div>
             </div>
             <label class="col-sm-2 col-form-label" for="dibuatoleh">Di Buat Oleh</label>
             <div class="col-sm-4">
-                <input type="text" class="form-control" id="dibuatoleh" name="dibuatoleh" 
-                    placeholder="Di Buat Oleh" value="{{ $beritaAcara->dibuatoleh }}" required>
+                <input type="text" class="form-control" id="dibuatoleh" name="dibuatoleh" placeholder="Di Buat Oleh"
+                    value="{{ $beritaAcara->dibuatoleh }}" required>
             </div>
         </div>
         <div class="form-group mb-3 row">
@@ -84,14 +72,9 @@
             <div class="col-sm-4">
                 <select class="form-select" name="penanda_tangan_id" required>
                     @foreach ($penandaTangan as $d)
-                    <option 
-                        value="{{ $d->id }}"
-                        @if ($beritaAcara->penanda_tangan_id == $d->id)
-                            selected
-                        @endif
-                    >
-                        {{ $d->name }}
-                    </option>
+                        <option value="{{ $d->id }}" @if ($beritaAcara->penanda_tangan_id == $d->id) selected @endif>
+                            {{ $d->name }}
+                        </option>
                     @endforeach
                 </select>
             </div>
@@ -99,12 +82,7 @@
             <div class="col-sm-4">
                 <select class="form-select" name="pihak_verifikasi_id" required>
                     @foreach ($pihakVerifikasi as $d)
-                        <option 
-                            value="{{ $d->id }}"
-                            @if ($beritaAcara->pihak_verifikasi_id == $d->id)
-                                selected
-                            @endif
-                        >
+                        <option value="{{ $d->id }}" @if ($beritaAcara->pihak_verifikasi_id == $d->id) selected @endif>
                             {{ $d->name }}
                         </option>
                     @endforeach
@@ -114,20 +92,15 @@
         <div class="form-group mb-3 row">
             <label class="col-sm-2 col-form-label" for="alasan">PIC</label>
             <div class="col-sm-4">
-                <input type="text" class="form-control" name="pic" placeholder="PIC" value="{{ $beritaAcara->pic }}">
+                <input type="text" class="form-control" name="pic" placeholder="PIC"
+                    value="{{ $beritaAcara->pic }}">
             </div>
             <label class="col-sm-2 col-form-label" for="keterangan">Keterangan</label>
             <div class="col-sm-4">
-                <textarea type="text" class="form-control" id="keterangan" name="keterangan" 
-                    placeholder="Keterangan" rows="2" required>{{ $beritaAcara->keterangan }}</textarea>
+                <textarea type="text" class="form-control" id="keterangan" name="keterangan" placeholder="Keterangan"
+                    rows="2" required>{{ $beritaAcara->keterangan }}</textarea>
             </div>
         </div>
-        {{-- <div class="form-group mb-3 row">
-            <label class="col-sm-2 col-form-label" for="lampiranpendukung">Lampiran Pendukung</label>
-            <div class="col-sm-10">
-                <input type="file" class="form-control" id="lampiranpendukung" name="lampiranpendukung" laceholder="Lampiran Pendukung" accept=".pdf">
-            </div>
-        </div> --}}
         <div class="row justify-content-end">
             <div class="col-1">
                 <button class="btn btn-primary w-100" type="submit">Kirim</button>
